e8729c1efff742ef32c1f5b9625fe85cab6bf46b
Andreas Villem <andreasvillem@hotmail.com>
2025-03-12 20:03:37 +0200

    initialize spring project with maven

---
+++ src/test/java/com/andvil/flight/recommendation/FlightRecommendationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andvil.flight.recommendation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightRecommendationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
